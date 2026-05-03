a69497a261b3a66a99dadf2c5410f91b9308c625
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2026-05-04 09:50:51 +1000

    Working on nav fix

--- streamlit_app/Home.py
+++ streamlit_app/Home.py
@@ -9,10 +9,10 @@ st.set_page_config(
 
 # Set up navigation with Material Icons
 # Icons at https://fonts.google.com/icons?icon.set=Material+Symbols
-homePage = st.Page("pages/home.py", title="Home", icon=":material/home:", default=True)
-calculatorPage = st.Page("pages/calculator.py", title="Risk Calculator", icon=":material/calculate:")
-faqsPage = st.Page("pages/faqs.py", title="FAQs", icon=":material/help:")
-contactPage = st.Page("pages/contact.py", title="Contact", icon=":material/mail:")
+homePage = st.Page("pages/_home.py", title="Home", icon=":material/home:", default=True)
+calculatorPage = st.Page("pages/_calculator.py", title="Risk Calculator", icon=":material/calculate:")
+faqsPage = st.Page("pages/_faqs.py", title="FAQs", icon=":material/help:")
+contactPage = st.Page("pages/_contact.py", title="Contact", icon=":material/mail:")
 
 pg = st.navigation([homePage, calculatorPage, faqsPage, contactPage])
 pg.run()
